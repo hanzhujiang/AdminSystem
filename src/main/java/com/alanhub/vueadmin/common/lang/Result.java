71d633d1aa35770a19a00aed97b52ba545eb44fd
AH <alan.han@vantagemarkets.com>
2022-08-30 11:44:03 +1000

    add fail respone

--- src/main/java/com/alanhub/vueadmin/common/lang/Result.java
+++ src/main/java/com/alanhub/vueadmin/common/lang/Result.java
@@ -3,8 +3,10 @@ package com.alanhub.vueadmin.common.lang;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class Result {
+public class Result implements Serializable {
     private int code;
     private String msg;
     private Object data;
@@ -13,6 +15,16 @@ public class Result {
         return  Result.succ(200,"success",data);
     }
     public static Result succ(int code,String msg,Object data){
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+    public static Result fail(Object data){
+        return  Result.succ(500,"failed",data);
+    }
+    public static Result fail(int code,String msg,Object data){
         Result result = new Result();
         result.setCode(code);
         result.setMsg(msg);
