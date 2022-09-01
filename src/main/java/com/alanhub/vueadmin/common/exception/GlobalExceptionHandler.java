5dd2d7756354e06f75d7ff22cb372cde9a6bba81
AH <alan.han@vantagemarkets.com>
2022-09-01 14:26:12 +1000

    add spring security

---
+++ src/main/java/com/alanhub/vueadmin/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.alanhub.vueadmin.common.exception;
+
+import com.alanhub.vueadmin.common.lang.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public Result handler(IllegalArgumentException e) {
+        log.error("Assert异常：----------------{}", e.getMessage());
+        return Result.fail(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = RuntimeException.class)
+    public Result handler(RuntimeException e){
+        log.error("运行时异常：----------------{}", e);
+        return Result.fail(e.getMessage());
+    }
+}
